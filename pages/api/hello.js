2a933aad32bcb43cdfdd5ed2902b08949394fcc7
Fran Agulto <fran.agulto@fran-agulto.attlocal.net>
2022-07-19 09:07:18 -0500

    added api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getSession } from "next-auth/react";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+
+  if (session) {
+    res.status(200).json({ name: "Hi Fran! This is your gated WP Content page here! Stoked" });
+  } else {
+    res.status(401).json({ error: "Don't come in here without your ID" });
+  }
 }
